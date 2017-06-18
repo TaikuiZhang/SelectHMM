85ef666ebcb47c9c75fbda009caa23f9c34afa95
Taikui Zhang <taikuizhang@126.com>
2017-06-18 21:34:08 +0800

    Update SelectHMM.pl

--- SelectHMM.pl
+++ SelectHMM.pl
@@ -27,6 +27,7 @@ $gfile=$gfile."_ref.fasta";
 open M,">$gfile";
 open N,">BadID_list.txt";
 my ($id,$seq);
+my $map;
 $/=">";
 my $b=0;
 <E>;
@@ -46,7 +47,7 @@ while(<E>){
 close E;
 close N;
 close M;
-print "\nCheck Genome-Protein Sequences: Done\n";
+#print "\nCheck Genome-Protein Sequences: Done\n";
 if ($b>0) {
     print "\nBad set of $b sequences with 'X' were filted out, and the bad IDs were list in BadID_list.txt.\n";
 	move("BadID_list.txt", "SelectHMM_Out");
@@ -55,14 +56,14 @@ if ($b>0) {
 else {
 	system("rm -f BadID_list.txt");
 }
-print "\nCheck Genome-Protein Sequences: Done.\n";
+print "\nCheck Genome-Protein Sequences: Done. See $gfile.\n";
 
 #HMMsearch
 my $file=(split(/.hmm/,$ARGV[ 1 ]))[0];
 $file=$file."_hmmout.txt";
 open P1,">$file";
 system("hmmsearch $ARGV[ 1 ] $gfile >$file");
-print "\nHMMsearch: Done.\n";
+print "\nHMMsearch: Done. See $file.\n";
 close P1;
 
 open P,"<$file";
@@ -72,27 +73,28 @@ my ($s1,$s2,$s3,$s4,$s5,$s6,$s7);
 my ($t1,$t2,$t3,$t4,$t5,$t6,$t7,$t8);
 my ($q1,$q2,$q3,$q4,$q5);
 print O "ID\tdomainN\tdomainS\tEvalue\n";
-$/="== domain";
+ $/="== domain";
 <P>;
 while(<P>){
 	chomp;
 	my ($s1,$s2,$s3,$s4,$s5,$s6,$s7)=split(/\n/,$_,7);
 	my ($t1,$t2,$t3,$t4,$t5,$t6,$t7,$t8)=split(/[\s]+/,$s1,8);
-	my ($q1,$q2,$q3,$q4,$q5)=split(/[\s]+/,$s5,5);
-	chomp($q2);
+	my ($q1,$q2,$q3,$q4,$q5)=split(/[\s]+/,$s4,5);
+	#$id=$q2."##D".$t2."S".$t4."E".$t8;
 	print O "$q2\t$t2\t$t4\t$t8\n";
 	print L "$q2\n";
-}
+ }
 close O;
 close P;
 close L;
-print "\nSummary HMMStat: Done.\n";
+print "\nSummary HMMStat: Done. See $file.Stat.txt.\n";
 move("$file.Stat.txt", "SelectHMM_Out");
 $t++;
-open(R, ">Final_Protein.fa") or die $!;
-system ("perl ExtractSequence.pl $gfile Final_Protein.fa") || die("\nExtractSequence.pl should be installed in the same dir as SelectHMM.pl.$!\n");
-close R;
 
+#open(R, ">Final_Protein.fa") or die $!;
+system ("perl ExtractSequence.pl $gfile Final_Protein.fa");
+#close R;
+#close L;
 move("Final_Protein.fa", "SelectHMM_Out");
 $t++;
 
@@ -112,8 +114,8 @@ if ($t eq 3) {
 	print "d.BadID_list.txt #Filted File \n";
 	move("$gfile", "SelectHMM_Out");
 }
-system("rm -f list.txt");
-system("rm -f $file");
+#system("rm -f list.txt");
+#system("rm -f $file");
 
 print "\nUse the data obtained by SelectHMM, please cite in your paper:\n";
 print "\nZhang TK, Liu CY, Zhang HY, Yuan ZH (2017).An integrated approach to identify Cytochrome P450 superfamilies in plant species within the Malvids. In: Proceedings of 2017 5th international conference on bioinformatics and computational biology. Zhang D eds. New York: ACM. Pp.11-16.\n\n";
